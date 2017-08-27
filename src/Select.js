966beaa92e965a0f6f6accfa01df0aab1207e963
Evgeny Belkin sunafajro@gmail.com <sunafajro@gmail.com>
2017-08-27 23:57:50 +0300

    добавлен компонент выпадающий список для выбора типа скидки

---
+++ src/Select.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+class Select extends React.Component {
+
+  dataSearch = (e) => {
+    let value = e.target.value;
+    let name = this.props.options.name;
+    this.props.update(name, value);
+  };
+
+  render() {
+    return (
+      <div className={ this.props.options.validation }>
+        <select
+          className="form-control input-sm"
+          onChange={this.dataSearch}
+          value={ this.props.options.term }>
+          { this.props.filter.map(item =>
+            <option key={ item.key } value={ item.key }>
+              { item.value }
+            </option>
+          ) }               
+        </select>
+      </div>
+    );
+  }
+};
+
+/* проверяем props */
+Select.propTypes = {
+  options: PropTypes.object.isRequired,
+  update: PropTypes.func.isRequired,
+  filter: PropTypes.array.isRequired
+}
+
+export default Select;
