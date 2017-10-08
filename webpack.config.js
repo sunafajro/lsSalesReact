dd65fc60ca76fc43d87fbceef0f9c1505c2e49b2
Evgeny Belkin <sunafajro@gmail.com>
2017-10-08 21:41:55 +0300

    заменен пресет вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'stage-2', 'react']
+          presets: ['env', 'stage-2', 'react']
         }
       }
     ]
